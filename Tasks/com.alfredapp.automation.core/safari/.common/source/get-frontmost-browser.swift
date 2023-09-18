8db1a8f52ff3ee6099e3c7f03d99c1bd79acf8e1
Vítor Galvão <info@vitorgalvao.com>
2023-09-18 23:32:35 +0100

    Browser Tasks: Get frontmost browser even when it is not frontmost tab

---
+++ Tasks/com.alfredapp.automation.core/safari/.common/source/get-frontmost-browser.swift
@@ -0,0 +1,34 @@
+#!/usr/bin/env swift
+
+import AppKit
+
+// Constants
+let browserNames: Set<String> = [
+  "Safari", "Webkit",
+  "Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge",
+]
+
+// Grab windows
+let windowList: CFArray? = CGWindowListCopyWindowInfo(
+  [.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID)
+
+guard let windows = windowList as? [[String: Any]] else { fatalError("Unable to get window list") }
+
+let firstMatchingBrowserWindow = windows.first { window in
+  // Ensure valid app names from windows
+  guard
+    window["kCGWindowLayer"] as? Int == 0,
+    let appName = window["kCGWindowOwnerName"] as? String
+  else { return false }
+
+  // Check if app is a supported browser
+  return browserNames.contains { appName.hasPrefix($0) }
+}
+
+// Output
+guard
+  let firstMatchingBrowserWindow,
+  let matchedBrowser = firstMatchingBrowserWindow["kCGWindowOwnerName"] as? String
+else { fatalError("Did not find a supported web browser") }
+
+print(matchedBrowser)
